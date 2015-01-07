32efe202a7ef3d80e4e08dd2a2d5daa90be4be86
Chichi Lalescu <clalesc1@jhu.edu>
2015-01-07 13:21:06 -0500

    add 3D curve functionality

--- py2asy/__init__.py
+++ py2asy/__init__.py
@@ -5,6 +5,24 @@
 import numpy as np
 import subprocess
 
+def curve3D_to_asy(
+        points,
+        color = (1, 0, 0)):
+    # vertices
+    asy_txt = ('real[] x;\n' +
+               'real[] y;\n' +
+               'real[] z;\n')
+    # store points
+    for i in range(points.shape[0]):
+        asy_txt += ('x[{0}] = {1};\n'.format(i, points[i, 0]) +
+                    'y[{0}] = {1};\n'.format(i, points[i, 1]) +
+                    'z[{0}] = {1};\n'.format(i, points[i, 2]))
+    asy_txt += ('draw(graph(x,y,z), ' +
+                'rgb{0}, '.format(str(color)) +
+                'render(compression = High, merge = true))' +
+                ';\n')
+    return asy_txt
+
 def triangulated_surface_to_asy(
         points,
         triangles,
@@ -23,6 +41,7 @@ def triangulated_surface_to_asy(
     # store points
     for i in range(points.shape[0]):
         asy_txt += 'V[{0}] = {1};\n'.format(i, str(tuple(points[i])))
+    # store triangles
     for i in range(triangles.shape[0]):
         asy_txt += 'T[{0}] = triface_{1};\n'.format(i, str(tuple(triangles[i])))
     asy_txt += ('draw(surface(T), ' +
@@ -37,6 +56,7 @@ def asy_to_pdf(
     with open(figname + '.asy', 'w') as outfile:
         outfile.write(
             "import three;\n" +
+            "import graph3;\n" +
             "size(20cm);\n" +
             "//currentprojection=perspective(250,-250,250);\n" +
             "currentlight=Viewport;\n\n")
