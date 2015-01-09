815b4f73d5c9fe5c0426fccfebee8f8ebd0039be
Chichi Lalescu <clalesc1@jhu.edu>
2015-01-09 16:47:18 -0500

    add test for vector field plot
    and we get teeny arrows...

---
+++ examples/vector_field.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python2
+#######################################################################
+#                                                                     #
+#  Copyright 2014 Cristian C Lalescu                                  #
+#                                                                     #
+#  This file is part of py3Dpdf.                                      #
+#                                                                     #
+#  py3Dpdf is free software: you can redistribute it and/or modify    #
+#  it under the terms of the GNU General Public License as published  #
+#  by the Free Software Foundation, either version 3 of the License,  #
+#  or (at your option) any later version.                             #
+#                                                                     #
+#  py3Dpdf is distributed in the hope that it will be useful,         #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of     #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      #
+#  GNU General Public License for more details.                       #
+#                                                                     #
+#  You should have received a copy of the GNU General Public License  #
+#  along with py3Dpdf.  If not, see <http://www.gnu.org/licenses/>    #
+#                                                                     #
+#######################################################################
+
+import numpy as np
+import py3Dpdf
+import py3Dpdf.tvtk_tools
+
+from base import get_turbulent_scalar
+
+def main():
+    fx = get_turbulent_scalar()[3]
+    fy = get_turbulent_scalar()[3]
+    x, y, z, fz = get_turbulent_scalar()
+    grid1D = np.linspace(
+        -np.pi, np.pi,
+        fx.shape[0],
+        endpoint = False)
+    if py3Dpdf.found_mathgl:
+        gr = py3Dpdf.npGraph()
+        gr.set_limits(
+            points = {'x': grid1D,
+                      'y': grid1D,
+                      'z': grid1D})
+        xx = py3Dpdf.array_to_mglData(x)
+        yy = py3Dpdf.array_to_mglData(y)
+        zz = py3Dpdf.array_to_mglData(z)
+        ffx = py3Dpdf.array_to_mglData(fx)
+        ffy = py3Dpdf.array_to_mglData(fy)
+        ffz = py3Dpdf.array_to_mglData(fz)
+        gr.Axis('xyzU')
+        gr.Box()
+        gr.Label('x', 'x', 0)
+        gr.Label('y', 'y', 0)
+        gr.Label('z', 'z', 0)
+        gr.Vect(xx, yy, zz, ffx, ffy, ffz)
+        gr.WritePNG('mgl_iso_test.png')
+        gr.WritePRC('mgl_iso_test.prc')
+    return None
+
+if __name__ == '__main__':
+    main()
+
