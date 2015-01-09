b2087cd88ab2e916418b1bfff82b929ab1ac2107
Chichi Lalescu <clalesc1@jhu.edu>
2015-01-09 06:15:01 -0500

    add npGraph class (inheriting mglGraph)

--- py3Dpdf/mgl.py
+++ py3Dpdf/mgl.py
@@ -47,3 +47,38 @@ def triangulated_surface_to_mglGraph(
     graph.TriPlot(tt, uu, vv, ww, color)
     return None
 
+class npGraph(mathgl.mglGraph):
+    def __init__(self):
+        super(npGraph, self).__init__()
+        self.empty = True
+        return None
+    def triangulated_surface(
+            self,
+            points = None,
+            triangles = None,
+            color = 'r'):
+        npoints = points.shape[0]
+        ntriangles = triangles.shape[0]
+        uu = mathgl.mglData(npoints)
+        vv = mathgl.mglData(npoints)
+        ww = mathgl.mglData(npoints)
+        for i in range(npoints):
+            uu[i] = points[i, 0]
+            vv[i] = points[i, 1]
+            ww[i] = points[i, 2]
+        tt = mathgl.mglData(ntriangles*3)
+        for i in range(ntriangles):
+            for j in range(3):
+                tt[i*3 + j] = triangles[i, j]
+        tt.Rearrange(3, ntriangles)
+        if not self.empty:
+            self.SetRange('x', uu, True)
+            self.SetRange('y', vv, True)
+            self.SetRange('z', ww, True)
+        else:
+            self.SetRange('x', uu)
+            self.SetRange('y', vv)
+            self.SetRange('z', ww)
+            self.empty = False
+        return self.TriPlot(tt, uu, vv, ww, color)
+
