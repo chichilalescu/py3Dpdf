45c76042f9af7cc47c2514642ca4abf80b1ecbe0
Chichi Lalescu <clalesc1@jhu.edu>
2015-01-09 22:00:19 -0500

    further tests of vect plot

--- examples/vector_field.py
+++ examples/vector_field.py
@@ -30,6 +30,12 @@ def main():
     fx = get_turbulent_scalar()[3]
     fy = get_turbulent_scalar()[3]
     x, y, z, fz = get_turbulent_scalar()
+    fx /= 10
+    fy /= 10
+    fz /= 10
+    #fx[:] = 0
+    #fy[:] = x
+    #fz[:] = 0
     grid1D = np.linspace(
         -np.pi, np.pi,
         fx.shape[0],
@@ -40,20 +46,27 @@ def main():
             points = {'x': grid1D,
                       'y': grid1D,
                       'z': grid1D})
-        xx = py3Dpdf.array_to_mglData(x)
-        yy = py3Dpdf.array_to_mglData(y)
-        zz = py3Dpdf.array_to_mglData(z)
+        mglgrid = py3Dpdf.array_to_mglData(grid1D)
         ffx = py3Dpdf.array_to_mglData(fx)
         ffy = py3Dpdf.array_to_mglData(fy)
         ffz = py3Dpdf.array_to_mglData(fz)
-        gr.Axis('xyzU')
+        gr.SetSize(2**10, 2**10)
+        gr.Axis('xyz')
         gr.Box()
         gr.Label('x', 'x', 0)
         gr.Label('y', 'y', 0)
         gr.Label('z', 'z', 0)
-        gr.Vect(xx, yy, zz, ffx, ffy, ffz)
-        gr.WritePNG('mgl_iso_test.png')
-        gr.WritePRC('mgl_iso_test.prc')
+        gr.Vect(mglgrid,
+                mglgrid,
+                mglgrid,
+                ffx,
+                ffy,
+                ffz,
+                '<',
+                "legend 'ha ha'; meshnum 8")
+        gr.Legend()
+        gr.WritePNG('mgl_vect_test.png')
+        gr.WritePRC('mgl_vect_test.prc')
     return None
 
 if __name__ == '__main__':
